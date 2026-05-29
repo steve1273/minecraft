16f4b144c139c0a7b61ef5edd1b10c5fb6cc8492
steve1273 <kristian2011@landakotsskoli.is>
2026-05-29 10:05:27 +0000

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,89 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+e 7 e 7 e 7 e 7 7 e e 7 e 7 e 7 
+e e e 7 e e e e 7 e e 7 e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e b e e e 
+e b e e e e e e b e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e b e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e e 
+e e e e e e b e e e e e e e b e 
+e e e e e e e e e e e e e e e e 
+e e b e e e e e e e b e e e e e 
+e e e e e e e e e e e e e e e e 
+`;
+            case "image2":
+            case "myImage0":return img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`;
+            case "image3":
+            case "random pickaxe":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f f . . . . 
+. . . . f 9 9 9 9 9 9 f . . . . 
+. . . f 9 9 9 6 9 9 f f . . . . 
+. . f 9 6 6 6 9 9 f f . . . . . 
+. f 9 9 6 9 6 9 f f . . . . . . 
+. f 9 9 6 6 6 9 f . . . . . . . 
+. f 9 6 9 9 9 9 f . . . . . . . 
+. f 9 9 9 f f f e f . . . . . . 
+. f 9 9 f f . . f e f . . . . . 
+. f 9 f f . . . . f e f . . . . 
+. f f f . . . . . . f e f . . . 
+. . . . . . . . . . . f e f . . 
+. . . . . . . . . . . . f e f . 
+. . . . . . . . . . . . . f e f 
+. . . . . . . . . . . . . . f f 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
